65e686edcc5a55e673865778f6404bc0ba302105
Lasse Jacobsen <blackbelt@live.dk>
2022-03-31 10:02:46 +0200

    API Database Refactor - StabilT

--- Booking.API/Program.cs
+++ Booking.API/Program.cs
@@ -2,9 +2,11 @@ using Booking.Application.Contract;
 using Booking.Application.Implementation;
 using Booking.Application.Infrastructure;
 using Booking.Domain.DomainServices;
+using Booking.Infrastructure.Data;
 using Booking.Infrastructure.DomainServicesImpl;
 using Booking.Infrastructure.Queries;
 using Booking.Infrastructure.RepositoriesImpl;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +23,11 @@ builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 builder.Services.AddScoped<IBookingDomainService, BookingDomainService>();
 
+builder.Services.AddDbContext<BookingContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection"), x =>
+    {
+        x.MigrationsAssembly("Booking.Web");
+    }));
 
 var app = builder.Build();
 
--- Booking.Infrastructure/DomainServicesImpl/BookingDomainService.cs
+++ Booking.Infrastructure/DomainServicesImpl/BookingDomainService.cs
@@ -1,13 +1,27 @@
-﻿using Booking.Domain.DomainServices;
+﻿using Booking.Application.Contract.Dtos;
+using Booking.Domain.DomainServices;
+using Booking.Infrastructure.Data;
+
 namespace Booking.Infrastructure.DomainServicesImpl;
 
 public class BookingDomainService : IBookingDomainService
 {
+    private readonly BookingContext context;
+
+    public BookingDomainService(BookingContext context)
+    {
+        this.context = context;
+    }
     IEnumerable<Domain.Entities.Booking> IBookingDomainService.GetExsistingBookings()
     {
         var bookings = new List<Domain.Entities.Booking>();
-        Database.Bookings.Values.ToList()
-            .ForEach(a => bookings.Add(new Domain.Entities.Booking(a.Id, a.Start, a.Slut)));
+
+        context.Bookings.ToList().ForEach(a => bookings.Add(new Domain.Entities.Booking(a.Id, a.Start, a.Slut)));
+
+
+        //Database.Bookings.Values.ToList()
+        //    .ForEach(a => bookings.Add(new Domain.Entities.Booking(a.Id, a.Start, a.Slut)));
+
         return bookings;
     }
 }
